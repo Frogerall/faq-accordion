ef1553fc6bf6c1591ac516b5e7e23437d4a5bda5
Frogerall <saliticoadrian@gmail.com>
2023-11-19 02:36:15 +0800

    added rotate transform on the icon

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let boxBtn = document.querySelectorAll(".box");
 let boxAccord = document.querySelectorAll(".accord");
+let boxArrow = document.querySelectorAll(".arrow");
 
 boxBtn.forEach((element, index) => {
  element.addEventListener("click", () => {
   boxAccord[index].classList.toggle("open");
+  boxArrow[index].classList.toggle("rotate");
  });
 });
