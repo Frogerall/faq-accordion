c153493ae9bca2375fc35c3ba593d0f6e3bafdcf
Frogerall <saliticoadrian@gmail.com>
2023-11-19 15:38:02 +0800

    fix rotate icon

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let boxBtn = document.querySelectorAll(".box");
 let boxAccord = document.querySelectorAll(".accord");
 let boxArrow = document.querySelectorAll(".arrow");
 
-boxBtn.forEach((element, index) => {
- element.addEventListener("click", () => {
+boxBtn.forEach((button, index) => {
+ button.addEventListener("click", () => {
   boxAccord[index].classList.toggle("open");
   boxArrow[index].classList.toggle("rotate");
  });
